c133c3eb77f65b1d93f8390418e3bcb372bc3387
Mizael Almeida <mzalmeida.a@gmail.com>
2020-09-06 23:13:11 -0300

    Added loop to send files as attachments - closes #MKTP1CS-15; added percentual variation from ini - closes #MKTP1CS-7

--- sender.js
+++ sender.js
@@ -49,22 +49,20 @@ async function listener(client) {
 async function massSend(client) {
 
     /* TODO:
-        - Maybe split text lines?
-        - Account for attachments
         - Add logger
      */
 
     // Load send list passed as --send argument
     let sendList = JSON.parse(fs.readFileSync(argv.list, encoding='utf-8'));
 
-    // Enumerate send dir text and attachment files from --dir argument
+    // Enumerates send dir text and attachment files from --dir argument
     // Attachment files will be sent in alphabetical order
     // Rename files before sending? meh
     let campaignContent = loadCampaignFiles(argv.dir);
-    //let campaign_text = [];
-    //let campaign_files = [];
 
-    // TODO: Add key to replace with sendlist info (e.g. [[fullname]] => John D.
+    // TODO: Add key to replace with sendlist info (e.g. [[fullname]] => John D).
+    // TODO: Add option to send links with preview
+    // TODO: Add option to send contacts
     let campaignText = readTextfromFiles(campaignContent.text);
 
     // Sleep for 5 seconds after init, before starting send job
@@ -82,7 +80,7 @@ async function massSend(client) {
         console.log("Retrieved profile data:");
         console.log(profile);
 
-        if (profile !== 404){
+        if (profile !== 404) {
             targetID = profile.id._serialized;
 
             console.log(`Target: ${contact.name} - ${profile.id.user}`);
@@ -94,32 +92,51 @@ async function massSend(client) {
                 console.log("Started typing");
 
                 await new Promise(resolve => {
-                    let typingTime = typeTime(message.length, settings.timeouts.typing);
-                    console.log(`Typing timeout is ${typingTime}`);
+                    let typingTime = typeTime(
+                        message.length,
+                        settings.timeouts.typing,
+                        settings.timeouts.typing_variance
+                    );
+                    console.log(`Typing timeout is ${typingTime}ms - sleeping`);
                     setTimeout(resolve, typingTime);
                 });
 
-                await client.sendText(targetID, `${message}`);
+                await client.sendText(targetID, message);
                 console.log(`Typed text: ${message}`);
 
                 client.stopTyping(targetID).then();
                 console.log("Stopped typing");
             }
 
+            for (let attachment of campaignContent.files){
+                console.log("Sending attachments")
+                client.sendFile(targetID, attachment, path.basename(attachment));
+                console.log(`Sent ${path.basename(attachment)} as file`);
+                await new Promise(resolve => {
+                    console.log(`Attachment timeout is ${settings.timeouts.between_files} seconds - sleeping`);
+                    setTimeout(resolve,
+                        parseInt(settings.timeouts.between_files) * 1000);
+                })
+            }
+
             console.log("Finished sending to contact");
 
+        }
+
+        else {
+            console.log("Invalid or nonexistant contact - skipping");
         }
     }
 }
 
 // Function for setting wait time to simulate human typing
 // Returns wait time in milliseconds
-function typeTime(textLength, CPM) {
+function typeTime(textLength, CPM, variance= 10) {
     // Allows for random variance of up to n%
     // TODO: Set variance percentage as ini parameter
     CPM = parseInt(CPM);
-    let minCPM = Math.ceil(CPM - ((CPM / 100) * 20));
-    let maxCPM = Math.floor(CPM + ((CPM / 100) * 20));
+    let minCPM = Math.ceil(CPM - ((CPM / 100) * variance));
+    let maxCPM = Math.floor(CPM + ((CPM / 100) * variance));
     let randomCPM = Math.floor(Math.random() * (maxCPM - minCPM + 1) + minCPM);
 
     // Use CPM to get seconds per character, then multiply by length of text
