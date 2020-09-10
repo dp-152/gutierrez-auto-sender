713e43c646c564270fd2af12449dbf2d16f0bad3
David Hericles Lima <davidhericlesl@gmail.com>
2020-09-10 01:00:56 -0300

    JSON to CSV changes, now it will write a CSV file. #MKTP1CS-2

--- reportlog.js
+++ reportlog.js
@@ -1,80 +1,60 @@
 const fs = require('fs')
+const path = require('path')
 
 class ReportLog {
-    /*
-        Local variables;
-    */
-    result = {}; // JSON Object
-    info = {}; // Infos Object
-    logs = []; // Logs Array Object
+
+    // Local variables;
     filepath; // File that will be generated
 
     /**
      * ReportLog constructor
      * @param {string}  filepath     filepath for the JSON file 
      */
-    constructor(filepath){
+    constructor(filepath) {
         this.filepath = filepath;
     }
 
     /**
-     * Function to fill local variable (info) with send details.
-     * 
-     * @param {string}  message     message that was sent to leads
-     * @param {array}   files       array with file path that was sent to leads
-     * @param {int}     total       amount leads that was sent using sendList
-     * @param {int}     success     amount of successful numbers that received the message
-     * @param {int}     failed      amount of failed numbers that couldn't receive the message
-     */
-    setInfo(message, files, total, success, failed) {
-        this.info = {
-            "data": new Date().toLocaleDateString('pt-br'),
-            "total": total,
-            "success": success,
-            "failed": failed,
-            "message": message,
-            "files": files
-        };
-    }
-
-    /**
-     * Function to fill local variable (logs) with information of what was sent.
-     * 
-     * @param {string}  num     phone number from the lead
-     * @param {bool}    status  status of message; true if message was sent, false if not.
-     * @param {string}  msg     callback message received from venom
+     * Function to write a CSV log file into the directory given by filepath
+     * --------------------------------------
+     * @param {string} num  phone number
+     * @param {bool} status sent status
+     * --------------------------------------
+     * @result {void} if was successful
+     * @result {error} if it got any error (it will stop the execution)
      */
 
-    pushLog(num, status, msg) {
-        this.logs.push({
-            "num": num,
-            "status": status,
-            "msg": msg
-        });
-    }
+    pushLog(num, status) {
 
-    /**
-     * Function to generate the log result, compiling both infos and logs.
-     * @returns {string}    JSON Formatted Text
-     */
+        // combine data into a csv format
+        var csvdata = [num, status].join(",");
+        var newline = "\r\n";
 
-    generate() {
-        this.result.infos = this.info;
-        this.result.logs = this.logs;
-        return JSON.stringify(this.result, null, 2);
-    }
+        if (fs.existsSync(this.filepath)) {
+            // file exists
+            // append the data to the file
+            try {
+                fs.appendFileSync(this.filepath, (csvdata + newline));
+            } catch (err) {
+                console.log(err);
+            }
+        } else {
+            // file doesn't exists
+            var csvcolumns = ("num,status" + newline);
 
-    /**
-     * Function to create the desired file given in the constructor.
-     */
+            // if the log dir doesn't exist, create it.
+            if (!fs.existsSync(path.dirname(this.filepath))) {
+                fs.mkdirSync(path.dirname(this.filepath));
+            }
 
-    create(){
-        try{
-            fs.writeFileSync(this.filepath, this.generate());
-        }catch(e){
-            console.log(e);
-            return false;
+            // place header files along with the first data
+            try {
+                fs.writeFileSync(this.filepath, (csvcolumns + csvdata + newline));
+            } catch (err) {
+                console.log(err);
+            }
         }
+
     }
 
 }
