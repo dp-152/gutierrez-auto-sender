0802370dde7afaf677f57336c2e1d385e359ae01
Mizael Almeida <mzalmeida.a@gmail.com>
2020-10-01 13:38:48 -0300

    Added log lines to functions

--- sys/core/familiar-dialogue.js
+++ sys/core/familiar-dialogue.js
@@ -9,7 +9,7 @@ const {
     makeIpsum,
     typeTime,
     percentualVariation,
-    roundToPrecision
+    pluralSuffix
 } = require("../helper");
 
 
@@ -23,25 +23,34 @@ const familiarList = JSON.parse(fs.readFileSync(
 // Function to initiate conversations with familiar accounts
 async function familiarStartConversation(client) {
 
+    logger.info(`{{FAMILIAR}}: Starting conversation routine`);
+
     // Determining the amount of targets to start a conversation with
     const targetAmount = randomInRange(2, 6);
+    logger.info(`{{FAMILIAR}}: Will speak to ${targetAmount} contacts out of ${familiarList.contacts.length}`);
 
     let excludeTarget = [];
 
     for (let i = 0; i < targetAmount; ++i) {
         // Taking a random target from send list
         let targetIndex = randomInRange(0, familiarList.contacts.length - 1);
+        logger.debug(`{{FAMILIAR}}: Selected target at index ${targetIndex}`);
         // Will loop until it finds a contact it has not sent to yet
-        while (excludeTarget.includes(targetIndex)){
+        while (excludeTarget.includes(targetIndex)) {
+            logger.debug(`{{FAMILIAR}}: Target has already been spoken to. Selecting new target.`)
             targetIndex = randomInRange(0, familiarList.contacts.length - 1);
+            logger.debug(`{{FAMILIAR}}: Selected target at index ${targetIndex}`);
         }
         // Returns a valid contact object
         const target = await client.getNumberProfile(familiarList.contacts[targetIndex].phone);
+        logger.info(`{{FAMILIAR}}: Target is ${target.id._serialized}`);
         excludeTarget.push(targetIndex);
+        logger.debug(`{{FAMILIAR}}: Pushed target to exclusion list`);
 
         await sendRandomMessages(client, target.id._serialized);
 
         await new Promise(r => setTimeout(r, percentualVariation(15, 15)));
+        logger.info('{{FAMILIAR}}: Sleeping for 15 seconds...')
     }
 }
 
@@ -50,11 +59,12 @@ async function familiarReply(client, message) {
     // TODO: Enforce a limit to reply routine (random number to determine if should send reply tag or not?)
 
     if (message.body.includes('{{REPLY}}')) {
+        logger.info(`{{FAMILIAR}}: Incoming reply tag from ${message.from} - Starting reply routine...`)
         await sendRandomMessages(
             client,
             message.from,
             randomInRange(2, 5),
-            roundToPrecision(Math.random()) === 1
+            randomInRange(0,1, 0) === 1
         );
     }
 }
@@ -62,6 +72,9 @@ async function familiarReply(client, message) {
 async function sendRandomMessages(client, target, maxMsg = 8, askForReply = true) {
     // Determining the amount of messages to send at once to current target
     const messageAmount = randomInRange(1, maxMsg);
+    logger.info(`{{FAMILIAR}}: Will send a total of ${messageAmount} ` +
+        `message${pluralSuffix(messageAmount, 's')} to current target.`);
+    logger.info(`{{FAMILIAR}}: Will ${askForReply ? '' : 'NOT '}ask for reply in this interaction`);
 
     // Loops for the amount of messages determined on the previous step
     for (let j = 0; j < messageAmount; ++j) {
@@ -72,18 +85,26 @@ async function sendRandomMessages(client, target, maxMsg = 8, askForReply = true
         if (j === messageAmount - 1 && askForReply)
             message += "{{REPLY}}";
 
+        logger.info(`{{FAMILIAR}}: Generated message: ${message}`);
+        logger.debug("{{FAMILIAR}}: Started typing");
         client.startTyping(target).then()
             .catch((err) => {
-                logger.error(err);
+                logger.error('{{FAMILIAR}}: Error trying to start typing - ' + err);
             });
 
         await new Promise(r => {
             const typingTime = typeTime(message.length, settings.timeouts.typing, settings.timeouts.typing_variance);
+            logger.info(`{{FAMILIAR}}: Typing timeout is ${typingTime}ms - sleeping`);
             setTimeout(r, typingTime);
         });
 
-        await client.sendText(target, message).catch(err => logger.error(err));
-        client.stopTyping(target).then().catch(err => logger.error(err));
+        await client.sendText(target, message).catch(err => {
+            logger.error('{{FAMILIAR}}: Error sending message - ' + err);
+        });
+        client.stopTyping(target).then().catch(err => {
+            logger.error('{{FAMILIAR}}: Error trying to stop typing - ' + err);
+        });
+        logger.debug("{{FAMILIAR}}: Stopped typing");
     }
 }
 
