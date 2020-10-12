cb9737c24ca228565568473d1d2b6734930a9e6b
Mizael Almeida <mzalmeida.a@gmail.com>
2020-10-12 18:03:43 -0300

    Filtering out group messages globally on listener; refactored relay conditional; disabled reply routine until enqueuing is implemented

--- sys/core/listener.js
+++ sys/core/listener.js
@@ -7,19 +7,26 @@ const { autoReply } = require('./auto-reply');
 async function listener(client) {
     logger.info("{{LISTENER}}: Running listener for account...");
     client.onMessage(message => {
+        if (message.from.includes('@g.us'))
+            return;
         logger.info(`{{LISTENER}}: Incoming message from ${message.from}...`);
         logger.info(`{{LISTENER}}: Message contents: ${message.body}`);
         // TODO: Add option to use regex match instead of string match
-        if (message.body === settings.relay.match && !message.from.includes('@g.us')) {
-            autoReply(client, message);
+        if (settings.relay.enabled) {
+            if (message.body === settings.relay.match) {
+                autoReply(client, message);
+            }
+
+            else {
+                logger.info("{{LISTENER}}: Message has not matched any criteria. Sending default reply...")
+                client.sendText(message.from, settings.relay.reply_default);
+            }
         }
+        /*
         else if (message.body.includes('{{REPLY}}')) {
             familiarReply(client, message);
         }
-        else {
-            logger.info("{{LISTENER}}: Message has not matched any  criteria. Sending default reply...")
-            client.sendText(message.from, settings.relay.reply_default);
-        }
+         */
     });
 }
 
